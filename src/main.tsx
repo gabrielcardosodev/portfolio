bbae90b64cd579072c1dbf09b6f00d8f7f6bc5e8
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-04-05 04:03:10 -0300

    feat: add global styles to the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import { App } from './App'
+
+import { globalStyles } from './UI/Stitches/index'
 
 ReactDOM.render(
   <React.StrictMode>
+    {globalStyles()}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
