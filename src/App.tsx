d0c8d65d9483690784e5aa8ad1fb57ccf3270bf2
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-04-05 23:40:40 -0300

    feat: add about description component to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar'
+import AboutDescription from './components/AboutDescription'
 
 export function App() {
   return (
     <>
       <Navbar />
+      <AboutDescription />
     </>
   )
 }
