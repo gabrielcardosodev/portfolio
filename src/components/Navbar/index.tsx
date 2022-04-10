b9f074254b66e0cd9300715f803c51a0141761e3
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-04-10 02:52:44 -0300

    refactor: refactor navbar to another logic

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,37 +6,42 @@ import logoImg from '../../assets/images/logo.svg'
 import * as S from './styles'
 
 const Navbar = () => {
-  const [navIsOpen, setNavIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleToggleNav = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleCloseNav = () => {
+    setIsOpen(false)
+  }
 
   return (
-    <S.NavContainer height={navIsOpen ? true : false}>
-      <S.NavbarInnerContainer>
-        <S.NavLogoContainer>
-          <a href="/">
-            <S.Logo src={logoImg} alt="Logo" />
-          </a>
-        </S.NavLogoContainer>
-        <S.NavLinksContainer>
-          <S.NavLinks>
-            <S.NavLink href="#about">Sobre</S.NavLink>
-            <S.NavLink href="#projects">Projetos</S.NavLink>
-            <S.NavLink href="#formation">Formação</S.NavLink>
-            <S.NavLink href="#techs">Habilidades</S.NavLink>
-          </S.NavLinks>
-        </S.NavLinksContainer>
-        <S.OpenLinksButton onClick={() => setNavIsOpen(!navIsOpen)}>
-          {navIsOpen ? <FaTimes /> : <FaBars />}
-        </S.OpenLinksButton>
-      </S.NavbarInnerContainer>
-      {navIsOpen && (
-        <S.NavOpenContainer>
-          <S.NavOpenLinks href="#about">Sobre</S.NavOpenLinks>
-          <S.NavOpenLinks href="#projects">Projetos</S.NavOpenLinks>
-          <S.NavOpenLinks href="#formation">Formação</S.NavOpenLinks>
-          <S.NavOpenLinks href="#techs">Habilidades</S.NavOpenLinks>
-        </S.NavOpenContainer>
-      )}
-    </S.NavContainer>
+    <S.Header>
+      <a href="/">
+        <img src={logoImg} alt="Logo" />
+      </a>
+      <S.NavContainer className={isOpen ? 'responsive_navbar' : ''}>
+        <S.NavLinks href="#about" onClick={handleCloseNav}>
+          Sobre
+        </S.NavLinks>
+        <S.NavLinks href="#projects" onClick={handleCloseNav}>
+          Projetos
+        </S.NavLinks>
+        <S.NavLinks href="#formation" onClick={handleCloseNav}>
+          Formação
+        </S.NavLinks>
+        <S.NavLinks href="#techs" onClick={handleCloseNav}>
+          Habilidades
+        </S.NavLinks>
+        <S.NavButton closeNavButton onClick={handleToggleNav}>
+          <FaTimes />
+        </S.NavButton>
+      </S.NavContainer>
+      <S.NavButton onClick={handleToggleNav}>
+        <FaBars />
+      </S.NavButton>
+    </S.Header>
   )
 }
 
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,148 +1,103 @@
-import { keyframes } from '@stitches/react'
 import { styled } from '../../UI/Stitches'
 
-export const NavContainer = styled('nav', {
-  width: '100%',
-  backgroundColor: '$primary',
+export const Header = styled('header', {
   display: 'flex',
-  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  height: '80px',
+  padding: '0 2rem',
+  backgroundColor: '$primary',
+  position: 'sticky',
+  top: 0,
+
+  img: {
+    width: '120px',
+    height: '120px'
+  },
 
   '@m': {
-    height: '120px !important'
+    height: '100px',
+
+    img: {
+      width: '160px',
+      height: '160px'
+    }
+  }
+})
+
+export const NavContainer = styled('nav', {
+  '@s': {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '4rem',
+    backgroundColor: '$primary',
+    transform: 'translateY(-100vh)',
+    transition: 'transform $medium ease-in-out',
+
+    '&.responsive_navbar': {
+      transform: 'translateY(0)'
+    }
+  }
+})
+
+export const NavLinks = styled('a', {
+  marginRight: '2rem',
+  color: '$secondary',
+  fontSize: '$large',
+  transition: 'color $medium ease-in-out',
+  position: 'relative',
+
+  '&::after': {
+    content: '""',
+    position: 'absolute',
+    left: '0',
+    bottom: '-1.5rem',
+    width: '0',
+    height: '2px',
+    background: '$secondary',
+    transition: 'width $medium ease-in-out'
+  },
+
+  '&:hover': {
+    color: '$tertiary',
+
+    '&::after': {
+      width: '100%',
+      background: '$tertiary'
+    }
+  },
+
+  '@m': {}
+})
+
+export const NavButton = styled('button', {
+  display: 'block',
+  padding: '$5',
+  cursor: 'pointer',
+  background: 'transparent',
+  border: 'none',
+  color: '$secondary',
+  outline: 'none',
+  fontSize: '$xlarge',
+
+  '@m': {
+    display: 'none'
   },
 
   variants: {
-    height: {
+    closeNavButton: {
       true: {
-        height: '100vh'
-      },
-      false: {
-        height: '120px'
+        position: 'absolute',
+        top: '2rem',
+        right: '2rem'
       }
     }
-  }
-})
-
-export const NavLogoContainer = styled('div', {
-  flex: '30%',
-  display: 'flex',
-  alignItems: 'center',
-  paddingLeft: '20px'
-})
-
-export const NavLinksContainer = styled('div', {
-  display: 'flex',
-  flex: '70%',
-  justifyContent: 'flex-end',
-  paddingRight: '20px',
-
-  '@m': {
-    alignItems: 'center'
-  }
-})
-
-export const NavbarInnerContainer = styled('div', {
-  width: '100%',
-  height: '120px',
-  display: 'flex'
-})
-
-export const NavLinks = styled('div', {
-  display: 'none',
-
-  '@m': {
-    display: 'flex'
-  }
-})
-
-export const NavLink = styled('a', {
-  color: '$secondary',
-  fontSize: '$large',
-  fontWeight: '$bold',
-  textDecoration: 'none',
-  marginRight: '20px',
-  transition: 'all $medium ease-in-out',
-  position: 'relative',
-
-  '&::after': {
-    content: '""',
-    width: '0',
-    height: '2px',
-    backgroundColor: '$tertiary',
-    position: 'absolute',
-    left: '0',
-    bottom: '-1rem',
-    transition: 'width $medium ease-in-out'
-  },
-
-  '&:hover': {
-    color: '$tertiary',
-
-    '&::after': {
-      width: '100%',
-      backgroundColor: '$tertiary'
-    }
-  }
-})
-
-export const NavOpenLinks = styled('a', {
-  color: '$secondary',
-  fontSize: '$large',
-  fontWeight: '$bold',
-  textDecoration: 'none',
-  marginRight: '20px',
-  transition: 'all $medium ease-in-out',
-  position: 'relative',
-
-  '&::after': {
-    content: '""',
-    width: '0',
-    height: '2px',
-    backgroundColor: '$tertiary',
-    position: 'absolute',
-    left: '0',
-    bottom: '-1rem',
-    transition: 'width $medium ease-in-out'
-  },
-
-  '&:hover': {
-    color: '$tertiary',
-
-    '&::after': {
-      width: '100%',
-      backgroundColor: '$tertiary'
-    }
-  }
-})
-
-export const Logo = styled('img', {
-  margin: '10px',
-  width: '180px',
-  height: 'auto'
-})
-
-export const OpenLinksButton = styled('button', {
-  margin: '3rem',
-  backgroundColor: '$primary',
-  border: 'none',
-  outline: 'none',
-  color: '$secondary',
-  fontSize: '$large',
-  cursor: 'pointer',
-
-  '@m': {
-    display: 'none'
-  }
-})
-
-export const NavOpenContainer = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  margin: 'auto',
-  gap: '3rem',
-
-  '@m': {
-    display: 'none'
   }
 })
